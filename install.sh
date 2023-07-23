124275aaf7f4f7dd763c64984daad290d2de0756
paxsonyang <140296848+paxsonyang@users.noreply.github.com>
2023-07-23 12:51:26 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # add stretch repo to sources list
-echo "deb http://archive.raspbian.org/raspbian stretch main" >> /etc/apt/sources.list
+echo "deb [trusted=yes] http://archive.raspbian.org/raspbian stretch main" >> /etc/apt/sources.list
 
 # update package list and install dependencies
 apt update
